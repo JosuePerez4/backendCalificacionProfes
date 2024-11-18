3876756d2f5a2ac2a5c32c8758eea47e0f6fec2c
JosuePerez4 <josueperezpg@ufps.edu.co>
2024-11-17 19:50:05 -0500

    agregado la herencia de las tablas usuario

--- src/main/java/proyecto/tercera/nota/entities/Usuario.java
+++ src/main/java/proyecto/tercera/nota/entities/Usuario.java
@@ -5,11 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Table;
 import lombok.Data;
 
 @Data
 @Entity
+@Inheritance(strategy = InheritanceType.JOINED)
 @Table(name="usuario")
 public class Usuario {
 	
